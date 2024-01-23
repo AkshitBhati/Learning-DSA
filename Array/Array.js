00e7afc2ca5604c472ffae2c74eb52298e748138
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-01-23 22:03:14 +0530

    starting array

---
+++ Array/Array.js
@@ -0,0 +1,10 @@
+//array traversing
+
+const data = [1,2,3,4,5,6,7,77,88,56,66,455]
+
+//accessing every element of an array with their index
+// for(let i=0; i<data.length; i++){
+//     document.write(`Array ${i} is ${data[i]} <br />`)
+// }
+
+//
