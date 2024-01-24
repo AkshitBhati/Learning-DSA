fa32a5f6a44d95ef24c3e07f087e88967826496d
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-01-24 22:07:53 +0530

    adding elements in array

--- Array/Array.js
+++ Array/Array.js
@@ -1,10 +1,23 @@
 //array traversing
 
-const data = [1,2,3,4,5,6,7,77,88,56,66,455]
+// const data = [1,2,3,4,5,6,7,77,88,56,66,455]
 
 //accessing every element of an array with their index
 // for(let i=0; i<data.length; i++){
 //     document.write(`Array ${i} is ${data[i]} <br />`)
 // }
 
-//
+//insert an element in an array
+const data = [1, 2, 3, 4, 5, 6, 7, 77, 88, 56, 66, 455];
+
+// Use splice to insert an element at index 5
+// data.splice(5, 0, 100);
+
+// document.write(data);
+// console.log(data);
+
+//Use for loop to add the element at the 3 index
+const newEl = 5
+for(let i=0; i<data.length; i++){
+    return data[i]
+}
