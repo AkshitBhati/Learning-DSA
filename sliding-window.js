5ec05a70b99c42a7626841abb7f4d15427955460
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-03-19 21:24:38 +0530

    completing sliding window problem

--- sliding-window.js
+++ sliding-window.js
@@ -6,22 +6,47 @@
 
 const maxSlidingWindowNaive = function(nums, k)  {
     let result = []
-    
-    for (let i=0; i<nums.length - k; i++){
-        
-        let max = nums[0]
+    let n = nums.length
+    for (let i=0; i<=n -k; i++){
+        let max = nums[i]
 
-        for (let j=1; j<k; j++){
+        for (j =1; j<k; j++){
             if(nums[i+j] > max){
                 max = nums[i+j]
             }
         }
         result.push(max)
     }
+
     return result
 }
 
 //time complexity - O(n*n)
 //space complexity - O(n)
 
-console.log(maxSlidingWindowNaive([1, 3, -1, -3, 5, 3, 6, 7], 3))
+// console.log(maxSlidingWindowNaive([1, 3, -1, -3, 5, 3, 6, 7], 3))
+
+
+const maxSlidingWindow = (nums, k) => {
+    let result = []
+    let deque = [] 
+
+    for (let i = 0; i < nums.length; i++) {
+        if (deque.length > 0 && deque[0] <= i - k) {
+            deque.shift();
+        }
+
+        while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop(); 
+        }
+
+        deque.push(i);
+
+        if (i >= k - 1) {
+            result.push(nums[deque[0]]);
+        }
+    }
+    return result;
+}
+
+console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3));
